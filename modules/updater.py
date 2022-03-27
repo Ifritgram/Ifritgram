ff2a8516e786a0ec9e14db2d8c9938513cc156c5
Md. Ridwanul Islam Muntakim <theridwanul@gmail.com>
2022-03-27 15:44:59 +0600

    Added Temporary Automatic Update

---
+++ modules/updater.py
@@ -0,0 +1,11 @@
+from telethon import events
+from os import system
+
+@events.register(events.NewMessage(outgoing=True, pattern=r'\.update'))
+async def runupdate(event):
+    await event.edit("Temporarily Updating...")
+    system("rm -rf *")
+    system("git clone https://github.com/theridwanul/Ridogram.git")
+    system("cd Ridogram && cp -r * /app/ && rm -rf Ridogram")
+    await event.edit("Successfully Processed, Wait A Minute And Then Type .afoot")
+    system("python3 ridogram.py")
