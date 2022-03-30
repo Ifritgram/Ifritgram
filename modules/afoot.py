225deeef713339274bb3d915c8cf2bb53d725d57
Md. Ridwanul Islam Muntakim <theridwanul@gmail.com>
2022-03-31 02:19:47 +0600

    User Details Bug Fixed

--- modules/afoot.py
+++ modules/afoot.py
@@ -11,4 +11,4 @@ async def runafoot(event):
     ridogramuserdetails = await event.get_sender()
     messagelocation = event.to_id
     ridogramimage = "https://telegra.ph/file/5f0e2d045243c6ab2a4a0.jpg"
-    await client.send_file(messagelocation, ridogramimage, caption=f"Dear {ridogramuserdetails.first_name},\nI'm glad to announce that Ridogram is able to assist you.\n\n╭─⊸ Ridogram Version: 0.6.0\n├─⊸ Python Version: {python_version()}\n╰─⊸ Telethon Version: {version.__version__}\n\nThank You,\nRidogram")
+    await client.send_file(messagelocation, ridogramimage, caption=f"Dear {ridogramuserdetails.first_name},\nI'm glad to announce that Ridogram is able to assist you.\n\n╭─⊸ Ridogram Version: 0.6.1\n├─⊸ Python Version: {python_version()}\n╰─⊸ Telethon Version: {version.__version__}\n\nThank You,\nRidogram")
