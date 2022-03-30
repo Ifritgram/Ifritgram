a2f66d30f2ee6a019765d326ea51e642d279f105
Md. Ridwanul Islam Muntakim <theridwanul@gmail.com>
2022-03-30 20:27:59 +0600

    Added Auto React

--- ridogram.py
+++ ridogram.py
@@ -1,4 +1,4 @@
-import modules.client, modules.afoot, modules.selfdestruct, modules.details, modules.bombing, modules.translate, modules.wikipedia, modules.accountlimit, modules.covidinformation, modules.userhistory, modules.iptrace, modules.generatepassword, modules.removemessage, modules.goodmorning, modules.goodafternoon, modules.goodevening, modules.goodnight, modules.telegraph, modules.removealldp, modules.imageaction, modules.removeallmessages, modules.removemessagesforcefully, modules.ban, modules.kick, modules.banall, modules.kickall, modules.calculator, modules.whois, modules.hosthunter, modules.useraccountupdate, modules.texttospeech, modules.secretmessage, modules.phonenumbertrace, modules.restoremessage, modules.ping, modules.emailvalidator, modules.worldclock, modules.urlshortener, modules.qrcode, modules.usernamevalidation, modules.imagetopdf, modules.currencyinformation, modules.googlesearch, modules.webpagescreenshot, modules.zipcodeinformation, modules.tvshowinformation, modules.quotly, modules.dictionary, modules.generatedata, modules.carbon, modules.creditcardchecker, modules.speedtest, modules.weather, modules.updater, modules.groupadminlist
+import modules.client, modules.afoot, modules.selfdestruct, modules.details, modules.bombing, modules.translate, modules.wikipedia, modules.accountlimit, modules.covidinformation, modules.userhistory, modules.iptrace, modules.generatepassword, modules.removemessage, modules.goodmorning, modules.goodafternoon, modules.goodevening, modules.goodnight, modules.telegraph, modules.removealldp, modules.imageaction, modules.removeallmessages, modules.removemessagesforcefully, modules.ban, modules.kick, modules.banall, modules.kickall, modules.calculator, modules.whois, modules.hosthunter, modules.useraccountupdate, modules.texttospeech, modules.secretmessage, modules.phonenumbertrace, modules.restoremessage, modules.ping, modules.emailvalidator, modules.worldclock, modules.urlshortener, modules.qrcode, modules.usernamevalidation, modules.imagetopdf, modules.currencyinformation, modules.googlesearch, modules.webpagescreenshot, modules.zipcodeinformation, modules.tvshowinformation, modules.quotly, modules.dictionary, modules.generatedata, modules.carbon, modules.creditcardchecker, modules.speedtest, modules.weather, modules.updater, modules.groupadminlist, modules.autoreact
 
 client = modules.client.client
 
@@ -163,6 +163,9 @@ with client as ridogram:
 
 with client as ridogram:
     ridogram.add_event_handler(modules.groupadminlist.runsa)
+
+with client as ridogram:
+    ridogram.add_event_handler(modules.autoreact.runar)
 
 client.start()
 print("Ridogram Started")
