256f087bcbc4fefd3da22f6730d31a462686c2eb
Md. Ridwanul Islam Muntakim <theridwanul@gmail.com>
2022-04-09 15:44:58 +0600

    User History Bug Fixed

--- modules/userhistory.py
+++ modules/userhistory.py
@@ -1,11 +1,9 @@
 from telethon import events
-from time import sleep
 from async_timeout import asyncio
 
 @events.register(events.NewMessage(outgoing=True, pattern=r'\.uh'))
 async def runuh(event):
     await event.edit("Ridogram Collect User Records...")
-    sleep(2)
     getinformation = await event.get_reply_message()
     targetid = getinformation.sender_id
     bot = "@SangMataInfo_bot"
@@ -18,10 +16,33 @@ async def runuh(event):
             except asyncio.TimeoutError:
                break
             infobotresponse.append(message.text)
+        datalenght = len(infobotresponse)
         try:
             if "No records found" in infobotresponse:
                 await event.edit("User Record Not Found")
             else:
-                await event.edit(f"{infobotresponse[0]}\n\n{infobotresponse[1]}\n\n{infobotresponse[2]}")
+                if datalenght == 3:
+                    if "🔗 Link To Profile" in infobotresponse:
+                        infobotresponse.remove("🔗 Link To Profile")
+                        await event.edit(f"{infobotresponse[0]}\n\n{infobotresponse[1]}")
+                    elif f"🔗 [Link To Profile](tg://user?id={targetid})" in infobotresponse:
+                        infobotresponse.remove(f"🔗 [Link To Profile](tg://user?id={targetid})")
+                        await event.edit(f"{infobotresponse[0]}\n\n{infobotresponse[1]}")
+                elif datalenght == 2:
+                    if "🔗 Link To Profile" in infobotresponse:
+                        infobotresponse.remove("🔗 Link To Profile")
+                        await event.edit(f"{infobotresponse[0]}")
+                    elif f"🔗 [Link To Profile](tg://user?id={targetid})" in infobotresponse:
+                        infobotresponse.remove(f"🔗 [Link To Profile](tg://user?id={targetid})")
+                        await event.edit(f"{infobotresponse[0]}")
+                elif datalenght == 1:
+                    if "🔗 Link To Profile" in infobotresponse:
+                        infobotresponse.remove("🔗 Link To Profile")
+                        await event.edit("User Record Not Found")
+                    elif f"🔗 [Link To Profile](tg://user?id={targetid})" in infobotresponse:
+                        infobotresponse.remove(f"🔗 [Link To Profile](tg://user?id={targetid})")
+                        await event.edit("User Record Not Found")
+                    else:
+                        await event.edit(f"{infobotresponse[0]}")
         except:
             pass
