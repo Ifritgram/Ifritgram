80356242a8281a92762ef3aa32f0ba0588ff3ac3
SharafatKarim <sharafat2004@gmail.com>
2023-09-30 21:47:58 +0600

    telethon will be installed (only for lightweightness)

--- string_session.py
+++ string_session.py
@@ -8,6 +8,7 @@ except:
     print("Setting up virtual environment...")
     if not os.path.isdir("venv"):
         os.system("python3 -m venv venv");
+    os.system("venv/bin/pip3 install telethon")
 
 api_id = int(input("Enter API ID: "))
 api_hash = input("Enter API Hash: ")
