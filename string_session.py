019ae64f92e4aaecf720671a60fec93d27a64ea8
iniridwanul <theridwanul@gmail.com>
2023-10-02 18:49:46 +0600

    Added pip automation and bug fixed

--- string_session.py
+++ string_session.py
@@ -1,14 +1,5 @@
-#!/venv/bin/python3
-import os # for installing telethon
-
-try:
-    from telethon.sync import TelegramClient
-    from telethon.sessions import StringSession
-except:
-    print("Setting up virtual environment...")
-    if not os.path.isdir("venv"):
-        os.system("python3 -m venv venv")
-    os.system("venv/bin/pip3 install telethon")
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
 
 api_id = int(input("Enter API ID: "))
 api_hash = input("Enter API Hash: ")
