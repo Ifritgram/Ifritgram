0760255b06d5445f2eb5f5194ac3c5f646cf7cbb
SharafatKarim <sharafat2004@gmail.com>
2023-10-08 21:22:31 +0600

    cross platform support

--- crowgram.sh
+++ crowgram.sh
@@ -8,6 +8,59 @@ NC='\033[0m'
 # Variables [FOR DEVS]
 URL="https://github.com/iniridwanul/Crowgram"
 
+archlinux() {
+    echo -e "${RED}Installing dependencies...${NC}"
+    sudo pacman -Syyu --noconfirm
+    sudo pacman -S --noconfirm --needed git python python-pip python-setuptools python-wheel python-virtualenv ffmpeg nodejs npm
+    echo -e "${GREEN}Dependencies installed.${NC}"
+}
+
+ubuntu() {
+    echo -e "${RED}Installing dependencies...${NC}"
+    sudo apt-get update
+    sudo apt-get upgrade -y
+    sudo apt-get install -y git python3 python3-pip python3-setuptools python3-wheel python3-virtualenv ffmpeg nodejs npm
+    echo -e "${GREEN}Dependencies installed.${NC}"
+}
+
+termux() {
+    echo -e "${RED}Installing dependencies...${NC}"
+    pkg update
+    pkg upgrade -y
+    pkg install -y git python python-pip python-setuptools python-wheel python-virtualenv ffmpeg nodejs npm
+    echo -e "${GREEN}Dependencies installed.${NC}"
+}
+
+other_os() {
+    echo -e "${RED}Please install the dependencies manually.${NC}"
+    echo -e "${GREEN}Dependencies:${NC}"
+    echo -e "${GREEN}1. Python Version 3+${NC}"
+    echo -e "${GREEN}2. Git${NC}"
+    echo -e "${GREEN}3. Python3-pip${NC}"
+    echo -e "${GREEN}4. NodeJS${NC}"
+}
+
+check_os() {
+    echo -e "${RED}What is your OS?${NC}"
+    echo -e "${GREEN}Enter 1 for Ubuntu/Debian${NC}"
+    echo -e "${GREEN}Enter 2 for Arch/Manjaro${NC}"
+    echo -e "${GREEN}Enter 3 for Termux${NC}"
+    echo -e "${GREEN}Enter 4 for Other${NC}"
+    read os
+
+    if [ $os == "1" ]; then
+        ubuntu
+    elif [ $os == "2" ]; then
+        archlinux
+    elif [ $os == "3" ]; then
+        termux
+    elif [ $os == "4" ]; then
+        other_os
+    else
+        echo -e "${RED}Invalid option.${NC}"
+        check_os
+    fi
+}
 # Check directory
 check_dir() {
     if [ ! -f "crowgram.sh" ]; then
