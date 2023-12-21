27641ddf4214371d370c548d1d1a80fbb003f8aa
iniridwanul <theridwanul@gmail.com>
2023-12-22 04:16:59 +0600

    Added AFK mode

--- crowgram/plugins/misc/__init__.py
+++ crowgram/plugins/misc/__init__.py
@@ -5,6 +5,7 @@ from .protection import add_contact, delete_contact, checking
 from .restricted import get_restricted_content
 from .logger import get_pm_log, get_mention_log
 from .who import check_user_history
+from .afk import activate_afk_mode, deactivate_afk_mode, check_afk_status, afk_notification
 from .sethelper import set_helper
 from .help import crowgram_help
 from .updater import run_updater
