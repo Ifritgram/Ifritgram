7fb9652c4b88e8fecdf477e2e9efee81ae599c1f
iniridwanul <theridwanul@gmail.com>
2024-10-15 11:52:10 +0600

    Error handling of Ping plugin has been done

--- ifritgram/plugins/default/ping.py
+++ ifritgram/plugins/default/ping.py
@@ -6,9 +6,12 @@ ifritgram = core.client.ifritgram
 
 @events.register(events.NewMessage(outgoing=True, pattern=r"\>ping"))
 async def ping_the_server(event):
-    await event.delete()
-    start = datetime.now()
-    end = datetime.now()
-    ms = (end - start).microseconds / 1000
-    chat = event.to_id
-    await ifritgram.send_message(chat, f"Pinged!\nLatency: {ms} ms")
+    try:
+        await event.delete()
+        start = datetime.now()
+        end = datetime.now()
+        ms = (end - start).microseconds / 1000
+        chat = event.to_id
+        await ifritgram.send_message(chat, f"Pinged!\nLatency: {ms} ms")
+    except:
+        pass
