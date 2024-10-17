2939be31da783fe2aa4d44abb9e852b2b77c32db
iniridwanul <theridwanul@gmail.com>
2024-10-17 22:59:14 +0600

    Added usage button to download self-destructive media

--- ifritgram/utils/buttons.py
+++ ifritgram/utils/buttons.py
@@ -13,7 +13,8 @@ categories_menu = [
 misc_menu = [
     [
         Button.inline("Ping", b'ping'),
-        Button.inline("Raid", b'raid')
+        Button.inline("Raid", b'raid'),
+        Button.inline("Timer", b'timer')
     ],
     [
         Button.inline("Main Menu", b'main_menu')
