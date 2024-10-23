6fc28cf536ab91717bca03dbd09b9cbe33be0d73
iniridwanul <theridwanul@gmail.com>
2024-10-23 20:41:35 +0600

    Plugins are loaded dynamically

--- ifritgram/core/handlers.py
+++ ifritgram/core/handlers.py
@@ -1,22 +1,10 @@
 import core.client
-from plugins import default, helper, fighter, misc
+from .load_events import ifritgram_events
 
 ifritgram = core.client.ifritgram
 
 def run_handlers():
-    with ifritgram as ifrit:
-        ifrit.add_event_handler(helper.execute_helper)
-        ifrit.add_event_handler(helper.query_response)
-        
-        ifrit.add_event_handler(default.ping_the_server)
-        ifrit.add_event_handler(default.set_helper)
-
-        ifrit.add_event_handler(fighter.find_opponent)
-        ifrit.add_event_handler(fighter.remove_opponent)
-        ifrit.add_event_handler(fighter.chat_fight)
-
-        ifrit.add_event_handler(misc.get_self_destructive_media)
-
+    ifritgram_events()
     ifritgram.start()
     print("Ifritgram Started")
     try:
