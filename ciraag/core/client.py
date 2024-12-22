eb99e53575d2a79ad24e5b873e38e00d118cf3c0
iniridwanul <theridwanul@gmail.com>
2024-12-22 18:30:18 +0600

    Added Assistant client for enhanced Help Menu interaction

--- ciraag/core/client.py
+++ ciraag/core/client.py
@@ -9,4 +9,12 @@ class Ciraag:
         self.string_session = environ["string_session"]
         self.client = TelegramClient(StringSession(self.string_session), self.api_id, self.api_hash)
 
+class CiraagHelper:
+    def __init__(self):
+        self.api_id = int(environ["api_id"])
+        self.api_hash = environ["api_hash"]
+        self.bot_token = environ["bot_token"]
+        self.ciraag_client = TelegramClient("ciraag", self.api_id, self.api_hash).start(bot_token=self.bot_token)
+
 ciraag = Ciraag().client
+ciraag_bot = CiraagHelper().ciraag_client
